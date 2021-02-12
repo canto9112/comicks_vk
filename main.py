d674fb6985fb7e1aac6224f63cf15dcb4a154082
canto9112 <cantoweb112@gmail.com>
2021-02-12 19:19:57 +0300

    Добавил функцию получения списка групп ВК

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import requests
 from pprint import pprint
-
-
-url = 'http://xkcd.com/353/info.0.json'
+from dotenv import load_dotenv
+import os
 
 
 def get_image_link(url):
     response = requests.get(url)
     response.raise_for_status()
     response_json = response.json()
-    return response_json['img']
+    image_link = response_json['img']
+    image_num = response_json['num']
+    author_comment = response_json['alt']
+
+    return image_link, image_num, author_comment
 
 
 def save_image(url, file_name):
@@ -20,6 +23,29 @@ def save_image(url, file_name):
         file.write(response.content)
 
 
-image_link = get_image_link(url)
+def get_group_vk(url, token, api_version, user_id):
+    params = {
+        'access_token': token,
+        'v': api_version,
+        'user_id': user_id
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    response_json = response.json()
+    pprint(response_json)
 
-save_image(image_link, '1')
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    xkdc_url = 'http://xkcd.com/353/info.0.json'
+    api_vk_url = 'https://api.vk.com/method/groups.get'
+
+    vk_api_version = 5.21
+    token_vk = os.getenv('PERSON_TOKEN_VK')
+    user_id_vk = os.getenv('USER_ID_VK')
+
+    # image_link, image_name, author_comment = get_image_link(xkdc_url)
+    # print(author_comment)
+    # save_image(image_link, image_name)
+    get_group_vk(api_vk_url, token_vk, vk_api_version, user_id_vk)
