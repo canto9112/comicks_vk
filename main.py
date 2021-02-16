96bd3de9b1c44d7653559c250f676fc76c886075
canto9112 <cantoweb112@gmail.com>
2021-02-16 17:30:05 +0300

    Обработка удалений картинок

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     vk_token = os.getenv('VK_PERSON_TOKEN')
     vk_group_id = os.getenv('VK_GROUP_ID')
 
-    image_name, author_comment = xkdc.save_random_comics()
-
-    vk_api.post_comics(image_name, author_comment, vk_api_version, vk_token, vk_group_id)
-
-    os.remove(image_name)
+    try:
+        image_name, author_comment = xkdc.save_random_comics()
+        vk_api.post_comics(image_name, author_comment, vk_api_version, vk_token, vk_group_id)
+    finally:
+        os.remove(image_name)
